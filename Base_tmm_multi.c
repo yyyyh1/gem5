1bfbf14accb4fa989209b254dfa1625e6a78ad0c
hong huang <honghuang2000@outlook.com>
2021-12-08 15:27:39 -0500

    add multi proceses

--- Base_tmm_multi.c
+++ Base_tmm_multi.c
@@ -34,6 +34,12 @@ static const int count = 1024;
 #define     N               64             // Square matrix dimension N*N
 #define     TILE            16                  // Tile size
 
+
+const int n     = N; 
+const int tile  = TILE;
+// alignas(64) int **c, **a, **b;
+alignas(64) int c[n][n], a[n][n], b[n][n]; 
+
 alignas(64) int lastJJ[P*16];         // the last computed iteration of k2 loop
 alignas(64) int lastII[P*16];         	// the last computed iteration of i loop
 alignas(64) int lastKK[P*16];         // the last computed iteration of k2 loop
@@ -45,6 +51,47 @@ alignas(64) int insideTxKK[P*16];               // Flag for entering logging reg
 uint64_t start, end;                            // Used for printing the ticks on real system
 
 
+void Initialize() 
+{
+    int i, j;
+
+    //    srand (time(NULL));    
+    srand48(0);
+    for(i=0; i<P; i++) {
+        lastJJ[i*16] = (i*(n/P))-tile;
+        lastII[i*16] = -tile;
+        lastKK[i*16] = -tile;
+        lastJJLog[i*16] = (i*(n/P))-tile;
+        lastIILog[i*16] = -tile;
+        lastKKLog[i*16] = -tile;
+        insideTxII[i*16] = 0;
+        insideTxKK[i*16] = 0;
+    }
+
+// #if USE_DYNAMIC_ARRAY
+//     a = (int**)calloc(N,sizeof(int*));
+//     b = (int**)calloc(N,sizeof(int*));
+//     c = (int**)calloc(N,sizeof(int*));
+//     for(int i=0;i<N;i++)
+//     {
+//         a[i] = (int*)calloc(N,sizeof(int));
+//         b[i] = (int*)calloc(N,sizeof(int));
+//         c[i] = (int*)calloc(N,sizeof(int));
+//     }
+// #endif
+    for (i=0; i<n; i++) {
+        for (j=0; j<n; j++) {
+            //a[i][j] = drand48();
+            //b[i][j] = drand48();
+            a[i][j] = 2;
+            b[i][j] = 3;
+            c[i][j] = 0;
+            //            a[i][j] = rand() % 20;
+            //            b[i][j] = rand() % 20;
+            //            c[i][j] = 0;
+        }
+    }
+}
 
 
 void* run (void* arg)
@@ -75,16 +122,23 @@ int main (int argc, char** argv)
     //     exit(1);
     // }
     // int P = atoi(argv[1]);
-
+    assert(N >= (P*TILE));
     printf("Starting %d threads...\n", P);
 
     //struct timeval startTime;
     //int startResult = gettimeofday(&startTime, NULL);
     //assert(startResult == 0);
     int i;
+    pthread_attr_t attr;
+
+    Initialize();
+
     pthread_t* threads = (pthread_t*)calloc(P, sizeof(pthread_t));
     assert(threads != NULL);
-    
+
+    /* Initialize thread attribute */
+    pthread_attr_init(&attr);
+    pthread_attr_setscope(&attr, PTHREAD_SCOPE_SYSTEM);//sys manages contention
 
     for (i = 1 ; i < P; i++) {
         int createResult = pthread_create(&threads[i], 
